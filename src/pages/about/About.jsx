75fec07b8eea71033fd60b8b0375430e91be4107
施雅馨 <shihyaxin@shiyaxindebijixingdiannao.local>
2024-11-04 00:46:36 +0800

    modify About page (add resume section)

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,8 @@
 import Info from '../../components/Info';
 import Stats from '../../components/Stats'
 import Skills from '../../components/Skills'
+import { resume } from '../../data';
+import ResumeItem from '../../components/ResumeItem'
 import { FaDownload } from 'react-icons/fa';
 import CV from '../../assets/steve-Cv.pdf'
 import "./about.css"
@@ -49,6 +51,30 @@ const About = () => {
             <Skills />
           </div>
 
+        </section>
+
+        <div className="separator"></div>
+        <section className="resume">
+          <h3 className="section__subtitle subtitle__center">
+            Experience & Education
+          </h3>
+          <div className="resume__container grid">
+            <div className="resume__data">
+              { resume.map((val) => {
+                if (val.category === 'experience'){
+                  return <ResumeItem key={val.id} {...val}/>;
+                }
+              })}
+            </div>
+
+            <div className="resume__data">
+              { resume.map((val) => {
+                if (val.category === 'education'){
+                  return <ResumeItem key={val.id} {...val}/>;
+                }
+              })}
+            </div>
+          </div>
         </section>
       </main>
     );
