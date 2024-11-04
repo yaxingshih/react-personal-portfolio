b84b16e7f819e5e3bdd7d8d44e0b5e0442c17d33
施雅馨 <shihyaxin@shiyaxindebijixingdiannao.local>
2024-11-05 00:54:13 +0800

    modify Portfolio page

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,8 +1,21 @@
+import { portfolio } from '../../data';
+import PortfolioItem from '../../components/PortfolioItem';
+import './portfolio.css'
+
 const Portfolio = () => {
     return (
-      <div>
-        Portfolio Page
-      </div>
+      <section className="portfolio section">
+        <h2 className="section__title">
+          My <span>Portfolio</span>
+        </h2>
+        <div className="portfolio__containter container grid">
+          {portfolio.map((item) => {
+            return (
+              <PortfolioItem key={item.id} {...item}/>
+            )
+          })}
+        </div>
+      </section>
     );
   };
   
